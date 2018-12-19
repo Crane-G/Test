5a05fb8e0c538eef5fe6ba96f738f07a9592dba0
admin <geqi1103953519@163.com>
2018-12-19 19:57:07 +0800

    第一次添加

---
+++ test/src/cn/itcast/Test01.java
@@ -0,0 +1,18 @@
+package cn.itcast;
+
+/**
+ * @auther geqi
+ * @date 2018/12/19 18:07
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
